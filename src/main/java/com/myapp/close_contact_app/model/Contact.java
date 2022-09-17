e260bfecba58ce880e16d23255df7789a3c9bed8
Naphatsakorn Khotsombat <n.khotsombat@mycit.ie>
2022-09-17 05:43:12 +0200

    Add contructor, getter, setter and to string to Contact class

--- src/main/java/com/myapp/close_contact_app/model/Contact.java
+++ src/main/java/com/myapp/close_contact_app/model/Contact.java
@@ -1,5 +1,64 @@
 package com.myapp.close_contact_app.model;
 
-public class Contact {
+import java.util.Date;
 
+public class Contact {
+    private int PersonId1;
+    private int PersonId2;
+    private Date dateContact;
+    
+    public Contact(int personId1, int personId2, Date dateContact) {
+        PersonId1 = personId1;
+        PersonId2 = personId2;
+        this.dateContact = dateContact;
+    }
+
+    /**
+     * @return the personId1
+     */
+    public int getPersonId1() {
+        return PersonId1;
+    }
+
+    /**
+     * @param personId1 the personId1 to set
+     */
+    public void setPersonId1(int personId1) {
+        PersonId1 = personId1;
+    }
+
+    /**
+     * @return the personId2
+     */
+    public int getPersonId2() {
+        return PersonId2;
+    }
+
+    /**
+     * @param personId2 the personId2 to set
+     */
+    public void setPersonId2(int personId2) {
+        PersonId2 = personId2;
+    }
+
+    /**
+     * @return the dateContact
+     */
+    public Date getDateContact() {
+        return dateContact;
+    }
+
+    /**
+     * @param dateContact the dateContact to set
+     */
+    public void setDateContact(Date dateContact) {
+        this.dateContact = dateContact;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact [PersonId1=" + PersonId1 + ", PersonId2=" + PersonId2 + ", dateContact=" + dateContact + "]";
+    }
+    
+    
 }

f536f78251710eb7e1f8a2fcaa22f647c3e73900
Earth Kuma <kruthai2540@gmail.com>
2022-10-16 18:42:56 +0200

    Add Consultant class - Add Entity annotation - Add constructors - Add setters and getters

---
+++ src/main/java/com/myapp/close_contact_app/model/Consultant.java
@@ -0,0 +1,51 @@
+package com.myapp.close_contact_app.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Consultant extends Person {
+    
+    private int cid;
+    Name consultant;
+    
+    public Consultant(int cid, Name consultant) {
+        super();
+        this.cid = cid;
+        this.consultant = consultant;
+    }
+
+    public Consultant() {
+        super();
+    }
+
+    /**
+     * @return the cid
+     */
+    public int getCid() {
+        return cid;
+    }
+
+    /**
+     * @param cid the cid to set
+     */
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    /**
+     * @return the consultant
+     */
+    public Name getConsultant() {
+        return consultant;
+    }
+
+    /**
+     * @param consultant the consultant to set
+     */
+    public void setConsultant(Name consultant) {
+        this.consultant = consultant;
+    }
+    
+    
+    
+}

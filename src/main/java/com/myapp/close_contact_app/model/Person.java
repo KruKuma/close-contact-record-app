04ea7a75db4df178c85de6cc7d21080190e9fb17
Earth Kuma <kruthai2540@gmail.com>
2022-10-16 22:54:41 +0200

    Modify Person class - Change constuctor - Add comments

--- src/main/java/com/myapp/close_contact_app/model/Person.java
+++ src/main/java/com/myapp/close_contact_app/model/Person.java
@@ -1,50 +1,85 @@
 package com.myapp.close_contact_app.model;
 
 public class Person {
+    
     private Name name;
     private String phone;
     private String email;
     private int id;
     
-    public Person() {
-        
-    }
-    
-    public Person(String firstName, String middleName, String lastName, String phone, String email, int id) {
-        this.name = new Name(firstName, middleName, lastName);
+    /**
+     * @param name
+     * @param phone
+     * @param email
+     * @param id
+     */
+    public Person(Name name, String phone, String email, int id) {
+        super();
+        this.name = name;
         this.phone = phone;
         this.email = email;
         this.id = id;
     }
 
-    public String getName() {
-        return name.getFirstName() + name.getMiddleName() + name.getLastName();
+    /**
+     * 
+     */
+    public Person() {
+        super();
     }
 
-    public void setName(String firstName, String middleName, String lastName) {
-        this.name = new Name(firstName, middleName, lastName);
+    /**
+     * @return the name
+     */
+    public Name getName() {
+        return name;
     }
 
+    /**
+     * @param name the name to set
+     */
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the phone
+     */
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * @param phone the phone to set
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    /**
+     * @return the email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * @param email the email to set
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @param id the id to set
+     */
     public void setId(int id) {
         this.id = id;
     }
